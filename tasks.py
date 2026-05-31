9bde005eaded2accd820d0e85d38ea7b7b8e6679
nivethalaxmi <nivethalakshmi0810@gmail.com>
2026-05-31 22:56:50 +0530

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,371 @@
+#tast 8
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("swapping number a:" ,a)
+print("swapping number b:",b)
+
+
+
+#total amount
+book1=40
+book2=40
+book3=55
+amount=book1+book2+book3
+print("total amount:",amount)
+
+#totalcost
+selling=5
+cost=10
+total=selling*cost 
+print("total cost of pen:",total)
+
+#average mark
+paper1=78
+paper2=85
+paper3=92
+average=paper1+paper2+paper3/3
+print("average mark is:",average)
+
+
+#pizza
+pizza=250
+price=pizza*4
+print("total price:",price)
+
+
+#worker
+perday=800
+salary=800*30
+print("salary:",salary)
+#6
+travels=150
+mile=150/5
+print("miles:",mile)
+
+#8
+student=500
+balance=500-320
+print("remaining balance:",balance)
+
+#7
+price=15000
+gst=15000*18/100
+total_amount=price+gst
+print("total_amot:",total_amount)''
+
+
+#9
+monthly=25000
+expenses=18000
+saving=monthly-expenses
+print("saving:",saving)
+
+#10
+length=20
+breadth=10
+area =length*breadth
+print("Area of reactanglr:",area)'
+
+
+#11
+tank=500
+used=125
+water=tank-used
+print("remaninig water:",water)
+
+#12
+
+tot_stud=48
+groups=6
+no_of_stud=tot_stud/groups
+print("students in each group:",no_of_stud)''
+
+#13
+shirts=2
+cost=750
+total=cost*shirts
+print("total cast:",total)
+
+#14
+travels=12
+days=7
+total=travels*days
+print("total distance:",total)
+
+
+#15
+salary=25000
+bonus=5000
+total=salary+bonus
+print("total salary:",total)' 
+#16
+one_juice=40
+amount=40*8
+print("amount of juices:",amount)
+
+#17
+deposit=10000
+bank=deposit*5/100
+print("interst amount:",bank)'
+
+#18
+scored1=45
+scored2=55
+scored3=70
+runs=scored1+scored2+scored3
+print("total scored:",runs)
+
+#19
+marks=450
+total=500
+total_marks=(100*450)/500
+print('percentage:',total_marks)
+
+#20
+travels=240
+hours=4
+speed=travels/hours
+print("total speed is:",speed)'''
+#tast 8
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("swapping number a:" ,a)
+print("swapping number b:",b)
+
+
+
+#total amount
+book1=40
+book2=40
+book3=55
+amount=book1+book2+book3
+print("total amount:",amount)
+
+#totalcost
+selling=5
+cost=10
+total=selling*cost 
+print("total cost of pen:",total)
+
+#average mark
+paper1=78
+paper2=85
+paper3=92
+average=paper1+paper2+paper3/3
+print("average mark is:",average)
+
+
+#pizza
+pizza=250
+price=pizza*4
+print("total price:",price)
+
+
+#worker
+perday=800
+salary=800*30
+print("salary:",salary)
+#6
+travels=150
+mile=150/5
+print("miles:",mile)
+
+#8
+student=500
+balance=500-320
+print("remaining balance:",balance)
+
+#7
+price=15000
+gst=15000*18/100
+total_amount=price+gst
+print("total_amot:",total_amount)''
+
+
+#9
+monthly=25000
+expenses=18000
+saving=monthly-expenses
+print("saving:",saving)
+
+#10
+length=20
+breadth=10
+area =length*breadth
+print("Area of reactanglr:",area)'
+
+
+#11
+tank=500
+used=125
+water=tank-used
+print("remaninig water:",water)
+
+#12
+
+tot_stud=48
+groups=6
+no_of_stud=tot_stud/groups
+print("students in each group:",no_of_stud)''
+
+#13
+shirts=2
+cost=750
+total=cost*shirts
+print("total cast:",total)
+
+#14
+travels=12
+days=7
+total=travels*days
+print("total distance:",total)
+
+
+#15
+salary=25000
+bonus=5000
+total=salary+bonus
+print("total salary:",total)' 
+#16
+one_juice=40
+amount=40*8
+print("amount of juices:",amount)
+
+#17
+deposit=10000
+bank=deposit*5/100
+print("interst amount:",bank)'
+
+#18
+scored1=45
+scored2=55
+scored3=70
+runs=scored1+scored2+scored3
+print("total scored:",runs)
+
+#19
+marks=450
+total=500
+total_marks=(100*450)/500
+print('percentage:',total_marks)
+
+#20
+travels=240
+hours=4
+speed=travels/hours
+print("total speed is:",speed)'''
+
+
+#tast1
+wallet=500
+wallet-=120
+print(" remaining walet:",wallet)
+
+#tast2
+salary=20000
+salary+=3000
+print(" updated salary is :",salary)
+
+#task3
+water_tank=150
+water_tank+=50
+print("total water level:",water_tank)
+
+#task4
+battery=90
+usage=25
+battery-=usage
+print("battery percentage:",battery)
+
+#task5
+initial_savings=2000
+initial_savings*=2
+print("savings:",initial_savings)
+
+#task6
+initialprice=850
+incre_price_bag=150
+initialprice+=incre_price_bag
+print("price is:",initialprice)
+
+#task7
+travels=120
+travels+=30
+print("total distance:",travels)
+
+#task8
+initialscore=95
+loses=15
+initialscore-=loses
+print("tottal score:",initialscore)
+
+#task9
+inital_number=12
+inital_number*=5
+print("a number is:",inital_number)
+
+#task10
+box=40
+box-=10
+print(' no.of.chocolates:',box)
+
+#task11
+account=15000
+account+=5000
+print('updated balance:',account)
+
+#task12
+students=60
+students-=5
+print("remaining students:",students)
+
+#task13
+salary=8000
+salary*=3
+print(" worker salary:",salary)
+
+#task14
+
+price=950
+price+=50
+print("final price:",price)
+
+#task15
+
+score=120
+score+=35
+print("total score:",score)
+
+#task16
+battery=100
+battery-=40
+print("remaining battery:",battery)
+
+#task17
+
+cows=25
+cows*=2
+print('total cows:',cows)
+
+#task18
+pens=75
+pens-=20
+print('remaining pens:',pens)
+
+#task19
+inital_marks=72
+inital_marks+=8
+print("total marks:",inital_marks)
+
+#task20
+inital_number=7
+inital_number*=10
+print("the number is:",inital_number)
+
+
