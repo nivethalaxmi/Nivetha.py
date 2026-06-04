823488c98167d477f80b00ceacacc85353eadc33
nivethalaxmi <nivethalakshmi0810@gmail.com>
2026-06-04 21:21:27 +0530

    Add files via upload

---
+++ loopstaks.py
@@ -0,0 +1,213 @@
+#task1
+x=[10,20,30,40,50]
+print(20 in x)
+
+#task2
+i=[10,20,30,40,50]
+print(100 in i)
+
+#task3
+name="Python"
+print("P"in name)
+
+#task4
+name="Python"
+print("z" not in name)
+''
+#task5
+fruits=["Apple","watermelon","banana"]
+print("Apple" in fruits)"''
+
+#task6
+for i in range(1,10):
+    print(i)
+#task7
+for i in range(5,15):
+    print(i)
+
+#task8
+for i in range(2,20,2):
+    print(i)
+
+#task9
+for i in range(1,20,2):
+    print(i)
+
+#task10
+for i in range(10,100,10):
+    print(i)
+
+#task11
+for i in range(1,11):
+    print(i,"x5=",i*5)
+
+
+#task12
+table=int(input("Enter the table "))
+for i in range(1,11):
+    print(i,"x",table, "=",i*table)
+
+#task13
+sum=1
+for i in range(1,10):
+    sum=sum+i
+print(sum)'
+
+#task14
+sum=1
+for i in range(1,20,2):
+    sum=sum+i
+print(sum)
+
+#task15
+for i in range(1,10):
+    print(i*i)
+
+#decrement for loop
+#task16
+for i in range(10,1,-1):
+    print(i)
+#task17
+for i in range(20,1,-1):
+    print(i)
+#task18
+for i in range(20,2,-2):
+    print(i)
+
+#task19
+for i in range(19,1,-2):
+    print(i)
+
+#task20
+count=0
+for i in range(10,0,-1):
+    count=count+1
+print(count)
+
+#task21
+for i in range(50,0,-5):
+    print(i)
+
+#task22
+x=int(input("Enter the number"))
+for i in range(x,0,-1):
+    print(i)'
+
+#task23
+for i in range(10,1,-1):
+    print(i,"x6=",i*6)
+
+#task24
+name="Python"
+for i in name:
+    print(i)
+
+#task25
+name=input("Enter your name")
+for i in name:
+    print(i,end="  ")
+
+#task26
+count=0
+x=input()
+for i in x:
+    count=count+1
+print(count)
+
+#task27
+
+x=input()
+count=0
+for ch in x:
+    if ch  in "aeiouAEIOU":
+        count=count+1
+print(count)'''
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
