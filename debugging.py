22b5b90f613444dc229d635eef9b76ef920fa60b
nivethalaxmi <nivethalakshmi0810@gmail.com>
2026-06-23 22:39:27 +0530

    Add files via upload

---
+++ debugging.py
@@ -0,0 +1,210 @@
+#1
+age=18
+if age>=18:
+    print('Eligible to Vote')
+#2
+num=10
+if num==10:
+    print('Number is 10')
+
+#3
+marks=75
+if marks==50:
+    print('Pass')
+#4
+num=-5
+if num>0:
+    print('Positive')
+elif num<0:
+    print('Negative')
+
+#5
+temperature=30
+if temperature>35:
+    print('Hot')
+elif temperature>25:
+    print('Warn')
+else:
+    print('Cool')
+
+#6
+username='admin'
+if username =='Admin':
+    print('Login Success')
+
+#7
+number=10
+if number>5:
+    print('Greater')
+else:
+    print('Smaller')
+#8
+value=100
+if value>50:
+    print('High')
+else:
+    print('Low')
+#9
+for i in range(5):
+    print(i)
+#10
+for i in range(1,6):
+    print(i)
+    
+#11
+i=11
+while i<=5:
+    print(i)
+    i-=1
+#12
+for i in range(5):
+    print(i)
+#13
+numbers=[10,20,30]
+for num in numbers:
+    print(num)
+#14
+for i in range(5):
+    print(i)
+#15
+i=1
+while i<=5:
+    print(i)
+    i+=1
+#16
+for i in range(1,11):
+    if i==5:
+        print('Found')
+#17
+name='Python'
+print(name[1])
+
+#18
+text='hello'
+print(text.upper())
+#19
+message='Welcome'
+print(message.lower())
+#20
+word='Python'
+word=word.replace(word[0],"J")
+print(word)
+#21
+name='Praveen'
+print(name.split( ))
+#22
+text='Python'
+for i in range(len(text)):
+    print(i)
+#22
+text='Python'
+print(text.split(","))
+#23
+language='Python'
+print(language)
+#24
+numbers=[100,20,30]
+print(numbers[1])
+#25
+number=[1,2,3]
+number.append(4)
+print(number)
+#26
+fruits=['Apple','Orange']
+fruits.append('banana')
+print(fruits)
+#27
+numbers=[10,20,30]
+numbers.remove(30)
+print(numbers)
+#28
+numbers=[10,20,30]
+print(len(numbers))
+#29
+names=['A','B','C','D']
+for i in range(len(names)):
+    print(names[i])
+#30
+numbers=[10,20,30]
+total=0
+for i in numbers:
+    total=total+i
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
